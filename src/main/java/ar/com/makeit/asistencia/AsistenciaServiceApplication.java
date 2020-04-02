7c2b1f38dc62e7b863489eda8f21fa365d67075a
c00saravim <matias.saravia@nbch.com.ar>
2020-04-02 16:36:46 -0300

    Inicio del proyecto

---
+++ src/main/java/ar/com/makeit/asistencia/AsistenciaServiceApplication.java
@@ -0,0 +1,13 @@
+package ar.com.makeit.asistencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsistenciaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsistenciaServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/com/makeit/asistencia/AsistenciaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.makeit.asistencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsistenciaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
